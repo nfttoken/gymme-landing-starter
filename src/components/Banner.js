b4715f6e855d6b1b8ece25d23ae8ef84aa1cfb43
nfttoken <nft721@outlook.com>
2022-11-13 03:59:29 +0000

    11-13-01

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
+
+// import data
+import { banner } from '../data'
 const Banner = () => {
-  return <div>Banner</div>;
+// destructure banner data
+const { titlePart1, titlePart2, subtitle, textBtn } = banner;
+
+  return 
+  <section className='bg-neutral-500 h-[790px]'>
+    <div container mx-auto h-full >
+      <div>
+        {/*text*/}
+        <div>
+          <h1 className='h1 text-white mb-8 '>
+            {titlePart1} <br /> 
+            <span className='text-primarg-200 '>{titlePart2}</span>
+          </h1>
+          <p className='max-w-[415px] text-body-md lg:text-bodg-lg mb-8'>
+            {subtitle}
+          </p>
+          <btn className='btn btn-sm lg:btn-lg btn-secondary'>
+            {textBtn}
+          </btn>
+        </div>
+
+        {/*image*/}
+        <div>
+          image
+        </div>
+      </div>
+    </div>
+  </section>
 };
 
 export default Banner;
